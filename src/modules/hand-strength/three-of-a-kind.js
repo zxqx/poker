c39fd596076e1fa43db9d975a2592b63a7184f4e
Zak Angelle <zaknotzak@gmail.com>
2017-06-29 02:25:37 -0500

    Clean up hand strength functions

--- src/modules/hand-strength/three-of-a-kind.js
+++ src/modules/hand-strength/three-of-a-kind.js
@@ -3,8 +3,7 @@ import groupBy from 'lodash.groupby';
 export function isThreeOfAKind(hand) {
   const groups = groupBy(hand, 'value');
 
-  return Object.keys(groups).some(key => {
-    const group = groups[key];
-    return group.length === 3;
-  }) && Object.keys(groups).length === 3;
+  return Object.keys(groups)
+    .some(key => groups[key].length === 3)
+    && Object.keys(groups).length === 3;
 }
--- src/modules/hand-strength/two-pair.js
+++ src/modules/hand-strength/two-pair.js
@@ -3,8 +3,7 @@ import groupBy from 'lodash.groupBy';
 export function isTwoPair(hand) {
   const groups = groupBy(hand, 'value');
 
-  return Object.keys(groups).filter(key => {
-    const group = groups[key];
-    return group.length === 2;
-  }).length === 2;
+  return Object.keys(groups)
+    .filter(key => groups[key].length === 2)
+    .length === 2;
 }
