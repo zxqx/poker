e0b5216521bb4d5eba5e9af79c130aa47a5dc9ed
andrewangelle <andrewangelle@gmail.com>
2017-06-29 12:22:44 -0500

    fixed high card module logic, added tests for breadth

--- src/modules/hand-strength/high-card.js
+++ src/modules/hand-strength/high-card.js
@@ -1,8 +1,9 @@
 import groupBy from 'lodash.groupBy';
-import { isFlush } from './flush';
+import { isLooselyFlush } from './flush';
+import { isLooselyStraight } from './straight'
 
 export function isHighCard (hand) {
   const groups = groupBy(hand, 'value');
 
-  return Object.keys(groups).length === 5 && !isFlush(hand);
+  return Object.keys(groups).length === 5 && !isLooselyFlush(hand) && !isLooselyStraight(hand);
 }
--- test/modules/hand-strength/high-card/high-card.hands.js
+++ test/modules/hand-strength/high-card/high-card.hands.js
@@ -123,8 +123,8 @@ export const highCard = [
       suit: 'hearts'
     },
     {
-      value: 5,
-      faceValue: '5',
+      value: 8,
+      faceValue: '8',
       suit: 'clubs'
     },
     {
@@ -198,4 +198,59 @@ export const nonHighCard =
       suit: 'clubs'
     }
   ],
+  [
+    {
+      value: 3,
+      faceValue: '3',
+      suit: 'spades'
+    },
+    {
+      value: 2,
+      faceValue: '2',
+      suit: 'hearts'
+    },
+    {
+      value: 4,
+      faceValue: '4',
+      suit: 'clubs'
+    },
+    {
+      value: 5,
+      faceValue: '5',
+      suit: 'diamonds'
+    },
+    {
+      value: 6,
+      faceValue: '6',
+      suit: 'diamonds'
+    }
+  ],
+  [
+    {
+      value: 3,
+      faceValue: '3',
+      suit: 'spades'
+    },
+    {
+      value: 2,
+      faceValue: '2',
+      suit: 'spades'
+    },
+    {
+      value: 4,
+      faceValue: '4',
+      suit: 'spades'
+    },
+    {
+      value: 5,
+      faceValue: '5',
+      suit: 'spades'
+    },
+    {
+      value: 6,
+      faceValue: '6',
+      suit: 'spades'
+    }
+  ],
+    
 ];
