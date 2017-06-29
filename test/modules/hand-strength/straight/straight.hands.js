e36ae92f88e7d6e1f0e317da80832131e1c9eaeb
Zak Angelle <zaknotzak@gmail.com>
2017-06-28 23:29:58 -0500

    Add isWheel check

--- test/modules/hand-strength/straight/straight.hands.js
+++ test/modules/hand-strength/straight/straight.hands.js
@@ -27,7 +27,6 @@ export const straights = [
     }
   ],
 
-
   [
     {
       value: 5,
@@ -55,6 +54,90 @@ export const straights = [
       suit: 'hearts'
     }
   ],
+
+  [
+    {
+      value: 11,
+      faceValue: 'J',
+      suit: 'spades'
+    },
+    {
+      value: 10,
+      faceValue: 'T',
+      suit: 'hearts'
+    },
+    {
+      value: 9,
+      faceValue: '9',
+      suit: 'clubs'
+    },
+    {
+      value: 7,
+      faceValue: '7',
+      suit: 'diamonds'
+    },
+    {
+      value: 8,
+      faceValue: '8',
+      suit: 'diamonds'
+    }
+  ],
+
+  [
+    {
+      value: 14,
+      faceValue: 'A',
+      suit: 'spades'
+    },
+    {
+      value: 10,
+      faceValue: 'T',
+      suit: 'hearts'
+    },
+    {
+      value: 11,
+      faceValue: 'J',
+      suit: 'clubs'
+    },
+    {
+      value: 13,
+      faceValue: 'K',
+      suit: 'diamonds'
+    },
+    {
+      value: 12,
+      faceValue: 'Q',
+      suit: 'diamonds'
+    }
+  ],
+
+  [
+    {
+      value: 2,
+      faceValue: '2',
+      suit: 'spades'
+    },
+    {
+      value: 14,
+      faceValue: 'A',
+      suit: 'hearts'
+    },
+    {
+      value: 5,
+      faceValue: '5',
+      suit: 'clubs'
+    },
+    {
+      value: 4,
+      faceValue: '4',
+      suit: 'diamonds'
+    },
+    {
+      value: 3,
+      faceValue: '3',
+      suit: 'diamonds'
+    }
+  ]
 ];
 
 export const nonStraights = [
@@ -112,6 +195,34 @@ export const nonStraights = [
       value: 10,
       faceValue: 'T',
       suit: 'hearts'
+    }
+  ],
+
+  [
+    {
+      value: 7,
+      faceValue: '7',
+      suit: 'spades'
+    },
+    {
+      value: 14,
+      faceValue: 'A',
+      suit: 'hearts'
+    },
+    {
+      value: 8,
+      faceValue: '8',
+      suit: 'clubs'
+    },
+    {
+      value: 10,
+      faceValue: '10',
+      suit: 'diamonds'
+    },
+    {
+      value: 9,
+      faceValue: '9',
+      suit: 'diamonds'
     }
   ],
 ];
