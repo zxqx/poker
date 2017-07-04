6af5c577495c4d74f3b6904f654910db7c8f9096
Zak Angelle <zaknotzak@gmail.com>
2017-07-03 21:30:26 -0500

    Refactor flush function

--- test/modules/hand-strength/flush/flush.hands.js
+++ test/modules/hand-strength/flush/flush.hands.js
@@ -1,180 +1,167 @@
- 
-export const flush  = [
-  [
-    {
-      value: 5,
-      faceValue: '5',
-      suit: 'spades'
-    },
-    {
-      value: 2,
-      faceValue: '2',
-      suit: 'spades'
-    },
-    {
-      value: 4,
-      faceValue: '4',
-      suit: 'spades'
-    },
-    {
-      value: 14,
-      faceValue: 'A',
-      suit: 'spades'
-    },
-    {
-      value: 10,
-      faceValue: 'T',
-      suit: 'spades'
-    }
-  ],
-
-
-  [
-    {
-      value: 8,
-      faceValue: '8',
-      suit: 'clubs'
-    },
-    {
-      value: 6,
-      faceValue: '6',
-      suit: 'clubs'
-    },
-    {
-      value: 7,
-      faceValue: '8',
-      suit: 'clubs'
-    },
-    {
-      value: 9,
-      faceValue: '9',
-      suit: 'clubs'
-    },
-    {
-      value: 11,
-      faceValue: 'J',
-      suit: 'clubs'
-    }
-  ],
+export const flush1 = [
+  {
+    value: 5,
+    faceValue: '5',
+    suit: 'spades'
+  },
+  {
+    value: 2,
+    faceValue: '2',
+    suit: 'spades'
+  },
+  {
+    value: 4,
+    faceValue: '4',
+    suit: 'spades'
+  },
+  {
+    value: 14,
+    faceValue: 'A',
+    suit: 'spades'
+  },
+  {
+    value: 10,
+    faceValue: 'T',
+    suit: 'spades'
+  }
 ];
 
-export const nonFlush = [
-  [
-    {
-      value: 3,
-      faceValue: '3',
-      suit: 'spades'
-    },
-    {
-      value: 14,
-      faceValue: 'A',
-      suit: 'hearts'
-    },
-    {
-      value: 4,
-      faceValue: '4',
-      suit: 'clubs'
-    },
-    {
-      value: 8,
-      faceValue: '5',
-      suit: 'clubs'
-    },
-    {
-      value: 6,
-      faceValue: '6',
-      suit: 'diamonds'
-    }
-  ],
+export const flush2 = [
+  {
+    value: 8,
+    faceValue: '8',
+    suit: 'clubs'
+  },
+  {
+    value: 6,
+    faceValue: '6',
+    suit: 'clubs'
+  },
+  {
+    value: 7,
+    faceValue: '8',
+    suit: 'clubs'
+  },
+  {
+    value: 9,
+    faceValue: '9',
+    suit: 'clubs'
+  },
+  {
+    value: 11,
+    faceValue: 'J',
+    suit: 'clubs'
+  }
+];
 
+export const nonFlush1 = [
+  {
+    value: 3,
+    faceValue: '3',
+    suit: 'spades'
+  },
+  {
+    value: 14,
+    faceValue: 'A',
+    suit: 'hearts'
+  },
+  {
+    value: 4,
+    faceValue: '4',
+    suit: 'clubs'
+  },
+  {
+    value: 8,
+    faceValue: '5',
+    suit: 'clubs'
+  },
+  {
+    value: 6,
+    faceValue: '6',
+    suit: 'diamonds'
+  }
+];
 
-  [
-    {
-      value: 5,
-      faceValue: '5',
-      suit: 'clubs'
-    },
-    {
-      value: 6,
-      faceValue: '6',
-      suit: 'clubs'
-    },
-    {
-      value: 8,
-      faceValue: '8',
-      suit: 'clubs'
-    },
-    {
-      value: 9,
-      faceValue: '9',
-      suit: 'diamonds'
-    },
-    {
-      value: 10,
-      faceValue: 'T',
-      suit: 'hearts'
-    }
-  ],
+export const nonFlush2 = [
+  {
+    value: 5,
+    faceValue: '5',
+    suit: 'clubs'
+  },
+  {
+    value: 6,
+    faceValue: '6',
+    suit: 'clubs'
+  },
+  {
+    value: 8,
+    faceValue: '8',
+    suit: 'clubs'
+  },
+  {
+    value: 9,
+    faceValue: '9',
+    suit: 'diamonds'
+  },
+  {
+    value: 10,
+    faceValue: 'T',
+    suit: 'hearts'
+  }
+];
 
+export const nonFlush3 = [
+  {
+    value: 9,
+    faceValue: '9',
+    suit: 'hearts'
+  },
+  {
+    value: 9,
+    faceValue: '9',
+    suit: 'clubs'
+  },
+  {
+    value: 9,
+    faceValue: '9',
+    suit: 'spades'
+  },
+  {
+    value: 2,
+    faceValue: '2',
+    suit: 'hearts'
+  },
+  {
+    value: 6,
+    faceValue: '6',
+    suit: 'diamonds'
+  },
+];
 
-
-  [
-    {
-      value: 9,
-      faceValue: '9',
-      suit: 'hearts'
-    },
-    {
-      value: 9,
-      faceValue: '9',
-      suit: 'clubs'
-    },
-    {
-      value: 9,
-      faceValue: '9',
-      suit: 'spades'
-    },
-    {
-      value: 2,
-      faceValue: '2',
-      suit: 'hearts'
-    },    
-    {
-      value: 6,
-      faceValue: '6',
-      suit: 'diamonds'
-    },
-
-  ],
-
-
-
-  [
-    {
-      value: 9,
-      faceValue: '9',
-      suit: 'hearts'
-    },
-    {
-      value: 9,
-      faceValue: '9',
-      suit: 'clubs'
-    },
-    {
-      value: 9,
-      faceValue: '9',
-      suit: 'spades'
-    },
-    {
-      value: 9,
-      faceValue: '2',
-      suit: 'diamonds'
-    },    
-    {
-      value: 6,
-      faceValue: '6',
-      suit: 'diamonds'
-    },    
-  ],
-
+export const nonFlush4 = [
+  {
+    value: 9,
+    faceValue: '9',
+    suit: 'hearts'
+  },
+  {
+    value: 9,
+    faceValue: '9',
+    suit: 'clubs'
+  },
+  {
+    value: 9,
+    faceValue: '9',
+    suit: 'spades'
+  },
+  {
+    value: 9,
+    faceValue: '2',
+    suit: 'diamonds'
+  },
+  {
+    value: 6,
+    faceValue: '6',
+    suit: 'diamonds'
+  }
 ];
