fe63c39c9ee32ea46fc69426b3dfe85d4ff670b9
Zak Angelle <zaknotzak@gmail.com>
2017-07-03 21:24:29 -0500

    Refactor four of a kind

--- test/modules/hand-strength/four-of-a-kind/four-of-a-kind.spec.js
+++ test/modules/hand-strength/four-of-a-kind/four-of-a-kind.spec.js
@@ -1,16 +1,26 @@
 import { isFourOfAKind } from '../../../../src/modules/hand-strength/four-of-a-kind';
-import { fourOfAKinds, nonFourOfAKinds } from './four-of-a-kind.hands.js';
+import {
+  fourOfAKind1,
+  fourOfAKind2,
+  nonFourOfAKind1,
+  nonFourOfAKind2
+} from './four-of-a-kind.hands.js';
 
 describe('four of a kind', () => {
   it('should determine hand is four of a kind', () => {
-    fourOfAKinds.forEach(hand => {
-      expect(isFourOfAKind(hand)).toBeTruthy();
+    expect(isFourOfAKind(fourOfAKind1)).toEqual({
+      values: [10],
+      kickers: [13]
+    });
+
+    expect(isFourOfAKind(fourOfAKind2)).toEqual({
+      values: [5],
+      kickers: [4]
     });
   });
 
   it('should determine hand is not four of a kind', () => {
-    nonFourOfAKinds.forEach(hand => {
-      expect(isFourOfAKind(hand)).toBe(false);
-    });
+    expect(isFourOfAKind(nonFourOfAKind1)).toBe(false);
+    expect(isFourOfAKind(nonFourOfAKind2)).toBe(false);
   });
 });
