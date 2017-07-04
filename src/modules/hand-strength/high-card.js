39d41fbdfd7962250099b575d81f0e3fc531f0c4
Zak Angelle <zaknotzak@gmail.com>
2017-07-03 21:45:24 -0500

    Refactor high card

--- src/modules/hand-strength/high-card.js
+++ src/modules/hand-strength/high-card.js
@@ -2,8 +2,17 @@ import groupBy from 'lodash.groupBy';
 import { isLooselyFlush } from './flush';
 import { isLooselyStraight } from './straight'
 
-export function isHighCard (hand) {
+export function isHighCard(hand) {
   const groups = groupBy(hand, 'value');
+  const keys = Object.keys(groups);
 
-  return Object.keys(groups).length === 5 && !isLooselyFlush(hand) && !isLooselyStraight(hand);
+  const criteria = keys.length === 5 &&
+    !isLooselyFlush(hand) && !isLooselyStraight(hand);
+
+  const values = keys.map(key => parseInt(key))
+    .sort((a, b) => b - a);
+
+  return criteria && {
+    values
+  };
 }
--- test/modules/hand-strength/high-card/high-card.hands.js
+++ test/modules/hand-strength/high-card/high-card.hands.js
@@ -1,256 +1,251 @@
-export const highCard = [
-  [
-    {
-      value: 13,
-      faceValue: 'K',
-      suit: 'spades'
-    },
-    {
-      value: 2,
-      faceValue: '2',
-      suit: 'hearts'
-    },
-    {
-      value: 4,
-      faceValue: '4',
-      suit: 'clubs'
-    },
-    {
-      value: 5,
-      faceValue: '5',
-      suit: 'diamonds'
-    },
-    {
-      value: 6,
-      faceValue: '6',
-      suit: 'diamonds'
-    }
-  ],
-
-  [
-    {
-      value: 5,
-      faceValue: '5',
-      suit: 'clubs'
-    },
-    {
-      value: 6,
-      faceValue: '6',
-      suit: 'clubs'
-    },
-    {
-      value: 8,
-      faceValue: '8',
-      suit: 'clubs'
-    },
-    {
-      value: 9,
-      faceValue: '9',
-      suit: 'diamonds'
-    },
-    {
-      value: 14,
-      faceValue: 'A',
-      suit: 'hearts'
-    }
-  ],
-
-  [
-    {
-      value: 11,
-      faceValue: 'J',
-      suit: 'spades'
-    },
-    {
-      value: 10,
-      faceValue: 'T',
-      suit: 'hearts'
-    },
-    {
-      value: 9,
-      faceValue: '9',
-      suit: 'clubs'
-    },
-    {
-      value: 2,
-      faceValue: '2',
-      suit: 'diamonds'
-    },
-    {
-      value: 8,
-      faceValue: '8',
-      suit: 'diamonds'
-    }
-  ],
-
-  [
-    {
-      value: 14,
-      faceValue: 'A',
-      suit: 'spades'
-    },
-    {
-      value: 6,
-      faceValue: '6',
-      suit: 'hearts'
-    },
-    {
-      value: 11,
-      faceValue: 'J',
-      suit: 'clubs'
-    },
-    {
-      value: 2,
-      faceValue: '2',
-      suit: 'diamonds'
-    },
-    {
-      value: 3,
-      faceValue: '3',
-      suit: 'clubs'
-    }
-  ],
-
-  [
-    {
-      value: 2,
-      faceValue: '2',
-      suit: 'spades'
-    },
-    {
-      value: 14,
-      faceValue: 'A',
-      suit: 'hearts'
-    },
-    {
-      value: 8,
-      faceValue: '8',
-      suit: 'clubs'
-    },
-    {
-      value: 4,
-      faceValue: '4',
-      suit: 'diamonds'
-    },
-    {
-      value: 3,
-      faceValue: '3',
-      suit: 'diamonds'
-    }
-  ]
+export const highCard1 = [
+  {
+    value: 13,
+    faceValue: 'K',
+    suit: 'spades'
+  },
+  {
+    value: 2,
+    faceValue: '2',
+    suit: 'hearts'
+  },
+  {
+    value: 4,
+    faceValue: '4',
+    suit: 'clubs'
+  },
+  {
+    value: 5,
+    faceValue: '5',
+    suit: 'diamonds'
+  },
+  {
+    value: 6,
+    faceValue: '6',
+    suit: 'diamonds'
+  }
 ];
 
-export const nonHighCard = 
-[
-  [
-    {
-      value: 3,
-      faceValue: '3',
-      suit: 'spades'
-    },
-    {
-      value: 8,
-      faceValue: '8',
-      suit: 'hearts'
-    },
-    {
-      value: 8,
-      faceValue: '8',
-      suit: 'clubs'
-    },
-    {
-      value: 3,
-      faceValue: '3',
-      suit: 'diamonds'
-    },
-    {
-      value: 8,
-      faceValue: '8',
-      suit: 'diamonds'
-    }
-  ],
+export const highCard2 = [
+  {
+    value: 5,
+    faceValue: '5',
+    suit: 'clubs'
+  },
+  {
+    value: 6,
+    faceValue: '6',
+    suit: 'clubs'
+  },
+  {
+    value: 8,
+    faceValue: '8',
+    suit: 'clubs'
+  },
+  {
+    value: 9,
+    faceValue: '9',
+    suit: 'diamonds'
+  },
+  {
+    value: 14,
+    faceValue: 'A',
+    suit: 'hearts'
+  }
+];
 
+export const highCard3 = [
+  {
+    value: 11,
+    faceValue: 'J',
+    suit: 'spades'
+  },
+  {
+    value: 10,
+    faceValue: 'T',
+    suit: 'hearts'
+  },
+  {
+    value: 9,
+    faceValue: '9',
+    suit: 'clubs'
+  },
+  {
+    value: 2,
+    faceValue: '2',
+    suit: 'diamonds'
+  },
+  {
+    value: 8,
+    faceValue: '8',
+    suit: 'diamonds'
+  }
+];
 
-  [
-    {
-      value: 5,
-      faceValue: '5',
-      suit: 'clubs'
-    },
-    {
-      value: 4,
-      faceValue: '4',
-      suit: 'clubs'
-    },
-    {
-      value: 8,
-      faceValue: '8',
-      suit: 'clubs'
-    },
-    {
-      value: 14,
-      faceValue: 'A',
-      suit: 'clubs'
-    },
-    {
-      value: 13,
-      faceValue: 'K',
-      suit: 'clubs'
-    }
-  ],
-  [
-    {
-      value: 3,
-      faceValue: '3',
-      suit: 'spades'
-    },
-    {
-      value: 2,
-      faceValue: '2',
-      suit: 'hearts'
-    },
-    {
-      value: 4,
-      faceValue: '4',
-      suit: 'clubs'
-    },
-    {
-      value: 5,
-      faceValue: '5',
-      suit: 'diamonds'
-    },
-    {
-      value: 6,
-      faceValue: '6',
-      suit: 'diamonds'
-    }
-  ],
-  [
-    {
-      value: 3,
-      faceValue: '3',
-      suit: 'spades'
-    },
-    {
-      value: 2,
-      faceValue: '2',
-      suit: 'spades'
-    },
-    {
-      value: 4,
-      faceValue: '4',
-      suit: 'spades'
-    },
-    {
-      value: 5,
-      faceValue: '5',
-      suit: 'spades'
-    },
-    {
-      value: 6,
-      faceValue: '6',
-      suit: 'spades'
-    }
-  ],
-    
+export const highCard4 = [
+  {
+    value: 14,
+    faceValue: 'A',
+    suit: 'spades'
+  },
+  {
+    value: 6,
+    faceValue: '6',
+    suit: 'hearts'
+  },
+  {
+    value: 11,
+    faceValue: 'J',
+    suit: 'clubs'
+  },
+  {
+    value: 2,
+    faceValue: '2',
+    suit: 'diamonds'
+  },
+  {
+    value: 3,
+    faceValue: '3',
+    suit: 'clubs'
+  }
+];
+
+export const highCard5 = [
+  {
+    value: 2,
+    faceValue: '2',
+    suit: 'spades'
+  },
+  {
+    value: 14,
+    faceValue: 'A',
+    suit: 'hearts'
+  },
+  {
+    value: 8,
+    faceValue: '8',
+    suit: 'clubs'
+  },
+  {
+    value: 4,
+    faceValue: '4',
+    suit: 'diamonds'
+  },
+  {
+    value: 3,
+    faceValue: '3',
+    suit: 'diamonds'
+  }
+];
+
+export const nonHighCard1 = [
+  {
+    value: 3,
+    faceValue: '3',
+    suit: 'spades'
+  },
+  {
+    value: 8,
+    faceValue: '8',
+    suit: 'hearts'
+  },
+  {
+    value: 8,
+    faceValue: '8',
+    suit: 'clubs'
+  },
+  {
+    value: 3,
+    faceValue: '3',
+    suit: 'diamonds'
+  },
+  {
+    value: 8,
+    faceValue: '8',
+    suit: 'diamonds'
+  }
+];
+
+export const nonHighCard2 = [
+  {
+    value: 5,
+    faceValue: '5',
+    suit: 'clubs'
+  },
+  {
+    value: 4,
+    faceValue: '4',
+    suit: 'clubs'
+  },
+  {
+    value: 8,
+    faceValue: '8',
+    suit: 'clubs'
+  },
+  {
+    value: 14,
+    faceValue: 'A',
+    suit: 'clubs'
+  },
+  {
+    value: 13,
+    faceValue: 'K',
+    suit: 'clubs'
+  }
+];
+
+export const nonHighCard3 = [
+  {
+    value: 3,
+    faceValue: '3',
+    suit: 'spades'
+  },
+  {
+    value: 2,
+    faceValue: '2',
+    suit: 'hearts'
+  },
+  {
+    value: 4,
+    faceValue: '4',
+    suit: 'clubs'
+  },
+  {
+    value: 5,
+    faceValue: '5',
+    suit: 'diamonds'
+  },
+  {
+    value: 6,
+    faceValue: '6',
+    suit: 'diamonds'
+  }
+];
+
+export const nonHighCard4 = [
+  {
+    value: 3,
+    faceValue: '3',
+    suit: 'spades'
+  },
+  {
+    value: 2,
+    faceValue: '2',
+    suit: 'spades'
+  },
+  {
+    value: 4,
+    faceValue: '4',
+    suit: 'spades'
+  },
+  {
+    value: 5,
+    faceValue: '5',
+    suit: 'spades'
+  },
+  {
+    value: 6,
+    faceValue: '6',
+    suit: 'spades'
+  }
 ];
